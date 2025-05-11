a89554d32239e9fc40620229e1f9e37a87b8c016
KhaledCS1 <10688@holbertonstudents.com>
2025-05-11 11:52:56 +0000

    Fix Task 1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -2,11 +2,10 @@
 
 from calculator_1 import add, sub, mul, div
 
-a = 10
-b = 5
-
 if __name__ == "__main__":
-print("{} + {} = {}" .format(a, b, add(a, b)))
-print("{} - {} = {}" .format(a, b, mul(a, b)))
-print("{} * {} = {}" .format(a, b, mul(a, b)))
-print("{} / {} = {}" .format(a, b, dif(a, b)))
+    a = 10
+    b = 5
+    print("{} + {} = {}" .format(a, b, add(a, b)))
+    print("{} - {} = {}" .format(a, b, mul(a, b)))
+    print("{} * {} = {}" .format(a, b, mul(a, b)))
+    print("{} / {} = {}" .format(a, b, dif(a, b)))
