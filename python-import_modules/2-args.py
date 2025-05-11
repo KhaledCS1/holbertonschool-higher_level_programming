5166f1d99ea30578335553cf5f1e4fb7abc5cf0d
KhaledCS1 <10688@holbertonstudents.com>
2025-05-11 17:10:01 +0000

    Added Task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     import sys
 
@@ -12,4 +11,3 @@ if __name__ == "__main__":
         print("{} arguments:".format(count))
     for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
-            
