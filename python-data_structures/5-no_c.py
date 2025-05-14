9e90c6201150409b6308dafb39a531468e4716eb
KhaledCS1 <10688@holbertonstudents.com>
2025-05-15 02:09:27 +0300

    Added Task 5

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+
+    khaled = " "
+    
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            kaled += char
+
+    
+    return khaled
