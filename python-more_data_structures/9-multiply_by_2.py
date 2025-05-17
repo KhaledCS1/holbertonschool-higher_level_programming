6433e0b16a930453cfcd3041614196aece03502e
KhaledCS1 <10688@holbertonstudents.com>
2025-05-17 10:51:26 +0300

    Added Tasks

---
+++ python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_dictionary = {}
+    for i in a_dictionary:
+        value = a_dictionary[i]
+        value_1 = value * 2
+        new_di-ctionary[i] = value_1
+    return new_dictionary
