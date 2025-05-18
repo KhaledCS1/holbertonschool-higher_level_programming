14b26e55c25b2e1ebc32bb7c6026303bc3c2217d
KhaledCS1 <10688@holbertonstudents.com>
2025-05-18 10:41:50 +0000

    Fix

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
     count = 0
     for i in range(x):
@@ -10,3 +9,4 @@ def safe_print_list(my_list=[], x=0):
             break
     print()
     return count
+
